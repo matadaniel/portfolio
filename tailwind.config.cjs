d71e0d4db2fe6378410b5b6590b8f2f668b015d9
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2022-11-16 11:14:26 -0600

    Style Layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,47 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['MontserratVariable', ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        appear: {
+          '0%': { opacity: 0, transform: 'translateX(20vw)' },
+          '100%': { opacity: 1 },
+        },
+        wave: {
+          '0%': { transform: 'rotate(90deg)' },
+        },
+      },
+      animation: {
+        appear: 'appear 2s',
+        wave: 'wave 2s cubic-bezier(1, 0, 0.5, 1)',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            ul: {
+              'max-width': 360,
+              display: 'grid',
+              'grid-template-columns': 'repeat(2, minmax(0, 1fr))',
+            },
+            a: { 'text-decoration-line': 'none' },
+            'ul > li > *:first-child': { margin: '1em 0' },
+            img: { margin: 0 },
+          },
+        },
+        xl: {
+          css: {
+            'ul > li > *:first-child': { margin: '1em 0' },
+            img: { margin: 0 },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
 }
