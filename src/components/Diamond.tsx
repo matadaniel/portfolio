1620f257e91895c760a4bf743d062654c9925db2
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2022-11-17 17:36:40 -0600

    Slow Diamond Rotation and change its color

--- src/components/Diamond.tsx
+++ src/components/Diamond.tsx
@@ -5,11 +5,11 @@ import { useFrame } from '@react-three/fiber'
 
 const Diamond = () => {
   const ref = useRef<Mesh>(null)
-  useFrame((state, delta) => (ref.current!.rotation.y += delta / 2))
+  useFrame((state, delta) => (ref.current!.rotation.y += delta / 4))
 
   return (
     <Octahedron scale={[1, 2, 1]} ref={ref}>
-      <meshPhongMaterial transparent opacity={0.5} color="black" side={DoubleSide} />
+      <meshPhongMaterial transparent opacity={0.5} color="rgb(20, 20, 20)" side={DoubleSide} />
     </Octahedron>
   )
 }
