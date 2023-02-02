ec8201928506fbe69328834d0a046b9d94ee5ec8
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2023-02-01 21:55:21 -0600

    Format with prettier-plugin-tailwindcss

--- src/components/Foundry.tsx
+++ src/components/Foundry.tsx
@@ -1,5 +1,5 @@
 const Foundry = () => (
-  <img src="/Foundry.png" className="inline align-text-top w-[1em]" alt="Foundry logo" />
+  <img src="/Foundry.png" className="inline w-[1em] align-text-top" alt="Foundry logo" />
 )
 
 export default Foundry
