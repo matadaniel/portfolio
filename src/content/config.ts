32e4966b2c7b1c1d21c76a3f0b3f0758695c8bdb
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2023-02-05 23:13:49 -0600

    Add drafts to schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,28 @@
 import { z, defineCollection } from 'astro:content'
 
-const blog = defineCollection({
-  schema: z.object({
+const dateString = z.string().transform(str => new Date(str))
+
+const baseBlogSchema = z
+  .object({
     title: z.string(),
     description: z.string(),
     tags: z.array(z.string()),
     series: z.string().optional(),
-    publishDate: z.string().transform(str => new Date(str)),
-    updateDate: z
-      .string()
-      .transform(str => new Date(str))
-      .optional(),
-  }),
+  })
+  .strict()
+
+const blog = defineCollection({
+  schema: z.discriminatedUnion('isDraft', [
+    baseBlogSchema.extend({
+      isDraft: z.literal(false),
+      publishDate: dateString,
+      updateDate: dateString.optional(),
+    }),
+    baseBlogSchema.extend({
+      isDraft: z.literal(true),
+      draftDate: dateString,
+    }),
+  ]),
 })
 
 export const collections = { blog }
