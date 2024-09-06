83688e9593789ae10d8b06ccd2740e6357cba8fe
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-09-06 16:48:00 -0500

    Change Diamond geometry

--- src/components/Diamond.tsx
+++ src/components/Diamond.tsx
@@ -1,16 +1,20 @@
 import { useRef } from 'react'
-import { Octahedron } from '@react-three/drei'
-import { DoubleSide, Mesh } from 'three'
+import { DoubleSide, Mesh, OctahedronGeometry } from 'three'
 import { useFrame } from '@react-three/fiber'
 
 const Diamond = () => {
   const ref = useRef<Mesh>(null)
   useFrame((state, delta) => (ref.current!.rotation.y += delta / 4))
 
+  const geometry = new OctahedronGeometry()
+  geometry.scale(1, 2, 1)
+
+  for (let i = 0; i < 4; i++) geometry.addGroup(i * 6, 6, i)
+
   return (
-    <Octahedron scale={[1, 2, 1]} ref={ref}>
+    <mesh geometry={geometry} ref={ref}>
       <meshPhongMaterial transparent opacity={0.5} color="rgb(79, 79, 79)" side={DoubleSide} />
-    </Octahedron>
+    </mesh>
   )
 }
 
