630ec7069f30b93bd9624ded492c8ba716161fa3
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-09-06 19:14:57 -0500

    Move Bulb into Diamond

--- src/components/Diamond.tsx
+++ src/components/Diamond.tsx
@@ -1,10 +1,36 @@
-import { useRef } from 'react'
-import { DoubleSide, Mesh, OctahedronGeometry } from 'three'
+import { useEffect, useRef, useState } from 'react'
+import { DoubleSide, Mesh, OctahedronGeometry, type PointLight } from 'three'
 import { useFrame } from '@react-three/fiber'
+import { Sphere } from '@react-three/drei'
 
 const Diamond = () => {
   const ref = useRef<Mesh>(null)
-  useFrame((state, delta) => (ref.current!.rotation.y += delta / 4))
+  const bulbRef = useRef<PointLight>(null)
+  const [scroll, setScroll] = useState(
+    window.scrollY / (document.body.offsetHeight - window.innerHeight)
+  )
+
+  const handleScroll = () => {
+    setScroll(window.scrollY / (document.body.offsetHeight - window.innerHeight))
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true })
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  useFrame((state, delta) => {
+    ref.current!.rotation.y += delta / 4
+
+    bulbRef.current!.position.set(
+      -1 * Math.cos(scroll * Math.PI),
+      2 * Math.sin(scroll * Math.PI) + 0.06,
+      -1 * Math.cos(scroll * Math.PI)
+    )
+  })
 
   const geometry = new OctahedronGeometry()
   geometry.scale(1, 2, 1)
@@ -12,9 +38,16 @@ const Diamond = () => {
   for (let i = 0; i < 4; i++) geometry.addGroup(i * 6, 6, i)
 
   return (
-    <mesh geometry={geometry} ref={ref}>
-      <meshPhongMaterial transparent opacity={0.5} color="rgb(79, 79, 79)" side={DoubleSide} />
-    </mesh>
+    <>
+      <pointLight color="white" ref={bulbRef} intensity={5}>
+        <Sphere args={[0.06, 16, 8]}>
+          <meshBasicMaterial color={0x7b7b7b} />
+        </Sphere>
+      </pointLight>
+      <mesh geometry={geometry} ref={ref}>
+        <meshPhongMaterial transparent opacity={0.5} color="rgb(79, 79, 79)" side={DoubleSide} />
+      </mesh>
+    </>
   )
 }
 
--- src/components/root.tsx
+++ src/components/root.tsx
@@ -11,7 +11,6 @@ import {
 } from 'three'
 import { extend, createRoot } from '@react-three/fiber'
 import Diamond from './Diamond'
-import Bulb from './Bulb'
 
 extend({
   PointLight,
@@ -40,7 +39,6 @@ document.getElementById('loading-bar')!.remove()
 
 root.render(
   <>
-    <Bulb />
     <directionalLight position={[-1, -1, 1]} intensity={6 * Math.PI} />
     <directionalLight position={[-1, 1, 1]} intensity={2 * Math.PI} />
     <pointLight position={[1, 2, 0]} intensity={4} color={0xacbbf4} />
